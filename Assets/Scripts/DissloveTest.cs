b670ee6fe098d2702381a3a9a52adaec0e72e3d7
Leeyunseok77 <qhdksrjp22@naver.com>
2024-09-26 15:19:17 +0900

    [Feat] Portal , Dissolve, Cloak 예제

---
+++ Assets/Scripts/DissloveTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissloveTest : MonoBehaviour
+{
+
+    public Material material;               //메테리얼을 받아온다.
+    public float amount = -1;               //설정한 값을 받아온다.
+    public bool bDisslove;                  //시작 플레그
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amount = -1;
+        material.SetFloat("_Amount", amount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bDisslove)
+        {
+            amount += Time.deltaTime;
+            material.SetFloat("_Amount", amount);
+        }
+
+    }
+}
